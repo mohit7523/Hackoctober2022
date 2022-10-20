ad69404bc426abfc4bb48c61447c78e35634bc5b
mathewtdpa <116245024+mathewtdpa@users.noreply.github.com>
2022-10-20 09:46:51 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,38 @@
+# Python 3 program to make GCD
+# of array a multiple of k.
+def MinOperation(a, n, k):
+	
+	result = 0
+	
+	for i in range(n) :
+	
+		''' If array value is not 1 and it
+		is greater than k then we can
+		increase the or decrease the
+		remainder obtained by dividing
+		k from the ith value of array so
+		that we get the number which is
+		either closer to k or its multiple '''
+		if (a[i] != 1 and a[i] > k) :
+			result = (result + min(a[i] % k,
+							k - a[i] % k))
+		
+		else :
+
+			# Else we only have one choice
+			# which is to increment the value
+			# to make equal to k
+			result = result + k - a[i]
+
+	return result
+
+# Driver code
+if __name__ == "__main__":
+	
+	arr = [ 4, 5, 6 ]
+	n = len(arr)
+	k = 5
+	print(MinOperation(arr, n, k))
+
+# This code is contributed
+# by ChitraNayal
